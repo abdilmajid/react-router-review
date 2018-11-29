8fa8450f88fe5ed74d909d695d3410854dc86f57
Abdilmajid Ibrahim <abdilmajidibrahim@gmail.com>
2018-11-29 16:12:38 -0500

    review react-router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Route from 'react-router-dom/Route';
+import User from './component/User'
 
 class App extends Component {
+//To use router need to wrap main tag with wrapper tag  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <Route path='/' exact render={() => {
+                return (
+                <h1>Welcome Home</h1>
+                )}}/>
+          <Route  path='/about' exact render={() => {
+                return (
+                <h1>About page</h1>
+                )}}/>
+          <Route  path='/users' exact render={() => {
+                return (
+                <User />
+                )}}/>            
+        </div>
+      </Router>
     );
   }
 }
+//Route-> '/' (default path) loads 'Welcome Home'
+//Route->'/about' (about page) loads
+// ** need to include 'exact' or multiple paths render
 
 export default App;
